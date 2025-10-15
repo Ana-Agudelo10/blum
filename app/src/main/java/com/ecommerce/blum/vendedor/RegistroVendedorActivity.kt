fd093f27488e2396a1895389828b2262d4a33cf0
Ana Agudelo <valeagudelob10@gmail.com>
2025-10-14 23:55:51 -0500

    Activity - Registro de Vendedor

---
+++ app/src/main/java/com/ecommerce/blum/vendedor/RegistroVendedorActivity.kt
@@ -0,0 +1,12 @@
+package com.ecommerce.blum.vendedor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ecommerce.blum.R
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+    }
+}
