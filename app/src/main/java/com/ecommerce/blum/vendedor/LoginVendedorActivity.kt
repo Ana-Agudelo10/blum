8f75eb122803040058600317f25d1c54d1901391
Harold0852 <avilaharol5@gmail.com>
2025-10-15 17:54:33 -0500

    Login funcional

--- app/src/main/java/com/ecommerce/blum/vendedor/LoginVendedorActivity.kt
+++ app/src/main/java/com/ecommerce/blum/vendedor/LoginVendedorActivity.kt
@@ -1,21 +1,90 @@
 package com.ecommerce.blum.vendedor
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.ecommerce.blum.R
+import com.ecommerce.blum.databinding.ActivityLoginVendedorBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
 
 class LoginVendedorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginVendedorBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_vendedor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginVendedorBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Por favor espere")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnLoginV.setOnClickListener{
+            validarInfo()
         }
+
+        binding.tvRegistrarV.setOnClickListener{
+            startActivity(Intent(applicationContext, RegistroVendedorActivity::class.java))
+        }
+
     }
+
+    private var email = ""
+    private var password = ""
+
+    private fun validarInfo() {
+        email=binding.etEmailV.text.toString().trim()
+        password=binding.etPasswordV.text.toString().trim()
+        if (email.isEmpty()){
+            binding.etEmailV.error = "Ingrese su email"
+            binding.etEmailV.requestFocus()
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmailV.error = "Ingrese un email valido"
+            binding.etEmailV.requestFocus()
+        }
+        else if (password.isEmpty()){
+            binding.etPasswordV.error = "Ingrese su contraseña"
+            binding.etPasswordV.requestFocus()
+        }
+        else{
+            loginVendedor()
+        }
+
+    }
+
+    private fun loginVendedor() {
+
+        progressDialog.setMessage("Iniciando sesión...")
+        progressDialog.show()
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                startActivity(Intent(this, MainActivityVendedor::class.java))
+                finishAffinity()
+                Toast.makeText(this, "Inicio de sesion exitoso", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e->
+                progressDialog.dismiss()
+                Toast.makeText(this, "No se pudo iniciar sesion debidoa a: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
 }
