6916217e58bf6a5f818c8ef1a6497ab5965bf5f5
Ana Agudelo <valeagudelob10@gmail.com>
2025-10-15 23:44:56 -0500

    Ajuste colorimetria

--- app/src/main/java/com/ecommerce/blum/SplashScreenActivity.kt
+++ app/src/main/java/com/ecommerce/blum/SplashScreenActivity.kt
@@ -18,7 +18,7 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun verBienvenida() {
-        object : CountDownTimer(3000, 1000) {
+        object : CountDownTimer(3000, 10) {
             override fun onTick(millisUntilFinished: Long) {
 
             }
