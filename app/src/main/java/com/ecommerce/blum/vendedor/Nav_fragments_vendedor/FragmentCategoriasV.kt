c0dc19c7c18a49559f4395bf3bb3e20855228aa3
Harold0852 <avilaharol5@gmail.com>
2025-10-28 19:50:54 -0500

    Fase 2: Crear categoria con imagen parte 1

--- app/src/main/java/com/ecommerce/blum/vendedor/Nav_fragments_vendedor/FragmentCategoriasV.kt
+++ app/src/main/java/com/ecommerce/blum/vendedor/Nav_fragments_vendedor/FragmentCategoriasV.kt
@@ -1,15 +1,19 @@
 package com.ecommerce.blum.vendedor.Nav_fragments_vendedor
 
+import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.ecommerce.blum.R
 import com.ecommerce.blum.databinding.FragmentCategoriasVBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
 import com.google.firebase.database.FirebaseDatabase
 
 class FragmentCategoriasV : Fragment() {
@@ -17,6 +21,8 @@ class FragmentCategoriasV : Fragment() {
     private lateinit var binding : FragmentCategoriasVBinding
     private lateinit var mContext : Context
     private lateinit var progressDialog : ProgressDialog
+
+    private var imageUri : Uri? = null
 
 
     override fun onAttach(context: Context) {
@@ -31,11 +37,41 @@ class FragmentCategoriasV : Fragment() {
         progressDialog.setTitle("Espere por favor")
         progressDialog.setCanceledOnTouchOutside(false)
 
+        binding.imgCategorias.setOnClickListener {
+            seleccionarImg()
+        }
+
         binding.btnAgregarCat.setOnClickListener {
             validarInfo()
         }
         return binding.root
     }
+
+    private fun seleccionarImg() {
+        ImagePicker.with(requireActivity())
+            .crop()	    			// Cortar la imagen
+            .compress(1024)			// comprimir la imagen a menos de 1 MB
+            .maxResultSize(1080, 1080)	// resolucion maximo 1080x1080
+            .createIntent { intent ->
+                resultadoImg.launch(intent)
+            }
+
+    }
+
+    private val resultadoImg =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ resultado->
+             if (resultado.resultCode == Activity.RESULT_OK){
+                 // Resultado OK
+                 val data = resultado.data
+                 imageUri = data!!.data
+                 // establecer la imagen seleccionada en el ImageView
+                 binding.imgCategorias.setImageURI(imageUri)
+             } else if (resultado.resultCode == ImagePicker.RESULT_ERROR){
+                 Toast.makeText(mContext, "Accion cacelada", Toast.LENGTH_SHORT).show()
+             } else {
+                 Toast.makeText(mContext, "Tarea cancelada", Toast.LENGTH_SHORT).show()
+             }
+        }
 
     private var categoria = ""
 
