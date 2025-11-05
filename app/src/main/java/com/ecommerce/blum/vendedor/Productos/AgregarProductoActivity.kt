dc4d5cfec96bc5e8f9b646d2c91f70ab46dd28df
Esthefania <estefa.naravez.26@gmail.com>
2025-11-04 22:29:42 -0500

    Fase 2.Agregar base de datos producto

--- app/src/main/java/com/ecommerce/blum/vendedor/Productos/AgregarProductoActivity.kt
+++ app/src/main/java/com/ecommerce/blum/vendedor/Productos/AgregarProductoActivity.kt
@@ -1,8 +1,10 @@
 package com.ecommerce.blum.vendedor.Productos
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
@@ -18,6 +20,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class AgregarProductoActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAgregarProductoBinding
@@ -28,12 +31,34 @@ class AgregarProductoActivity : AppCompatActivity() {
 
     private lateinit var caegoriasArrayList: ArrayList<ModeloCategoria>
 
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarProductoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         cargarCategorias()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.etPrecioConDescuentoP.visibility = View.GONE
+        binding.etNotaDescuentoP.visibility = View.GONE
+
+        binding.descuentoSwitch.setOnCheckedChangeListener{buttonView, isChecked ->
+            if(isChecked){
+                //Si el Switch esta habilitado
+                binding.etPrecioConDescuentoP.visibility = View.VISIBLE
+                binding.etNotaDescuentoP.visibility = View.VISIBLE
+
+            }else{
+                //SI el Switch esta deshabilitado
+                binding.etPrecioConDescuentoP.visibility = View.GONE
+                binding.etNotaDescuentoP.visibility = View.GONE
+            }
+        }
 
         imagenSelecArrayList = ArrayList()
 
@@ -42,13 +67,169 @@ class AgregarProductoActivity : AppCompatActivity() {
         }
 
 
-        binding.Categria.setOnClickListener {
+        binding.Categoria.setOnClickListener {
             selecCategorias()
+        }
+        binding.btnAgregarProducto.setOnClickListener {
+            validarInfo()
         }
 
         cargarImagenes()
 
     }
+
+    private var nombreP=""
+    private var descripcionP=""
+    private var categoriaP =""
+    private var precioP=""
+    private var descuentoHab= false
+    private var precioDescP = ""
+    private var notaDescP = ""
+    private fun validarInfo() {
+        nombreP = binding.etNombresP.text.toString().trim()
+        descripcionP = binding.etDescripcionP.text.toString().trim()
+        categoriaP = binding.Categoria.text.toString().trim()
+        precioP = binding.etPrecioP.text.toString().trim()
+        descuentoHab = binding.descuentoSwitch.isChecked
+
+        if(nombreP.isEmpty()){
+            binding.etNombresP.error = "Ingrese Nombre"
+            binding.etNombresP.requestFocus()
+
+        }
+        else if(descripcionP.isEmpty()){
+            binding.etDescripcionP.error = "Ingrese Descripcion"
+            binding.etDescripcionP.requestFocus()
+
+        }
+        else if(categoriaP.isEmpty()){
+            binding.Categoria.error = "Seleccione Una Catergoria"
+            binding.Categoria.requestFocus()
+
+        }
+        else if(precioP.isEmpty()){
+            binding.etPrecioP.error = "Seleccione un precio"
+            binding.etPrecioP.requestFocus()
+
+        }
+        else if(imagenUri == null){
+            Toast.makeText(this, "Seleccione al menos una imagen", Toast.LENGTH_SHORT).show()
+
+        }else{
+            //Descuento Habilitado = true
+            if (descuentoHab){
+
+                precioDescP = binding.etPrecioConDescuentoP.text.toString().trim()
+                notaDescP = binding.etNotaDescuentoP.text.toString().trim()
+                if (precioDescP.isEmpty()){
+                    binding.etPrecioConDescuentoP.error = "Ingrese precio con descuento"
+                    binding.etPrecioConDescuentoP.requestFocus()
+
+                }else if(notaDescP.isEmpty()){
+                    binding.etNotaDescuentoP.text.toString().trim()
+                    binding.etNotaDescuentoP.requestFocus()
+                }else{
+                    agregarProducto()
+                }
+
+            }
+            //Descuento deshabilitado = False
+            else{
+                precioDescP = "0"
+                notaDescP = ""
+                agregarProducto()
+
+            }
+
+
+        }
+
+    }
+
+    private fun agregarProducto() {
+        progressDialog.setMessage("Agregando Producto")
+        progressDialog.show()
+
+        var ref = FirebaseDatabase.getInstance().getReference("Productos")
+        val keyId = ref.push().key
+
+        val hashMap = HashMap<String, Any>()
+        hashMap["id"] = "${keyId}"
+        hashMap["nombre"] = "${nombreP}"
+        hashMap["descripcion"] = "${descripcionP}"
+        hashMap["categoria"] = "${categoriaP}"
+        hashMap["precio"] = "${precioP}"
+        hashMap["precioDesc"] = "${precioDescP}"
+        hashMap["notaDesc"]="${notaDescP}"
+
+        ref.child(keyId!!)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+
+                subirImgsStorage(keyId)
+
+            }
+            .addOnFailureListener {e->
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+    }
+    private fun subirImgsStorage(keyId: String) {
+        for (i in imagenSelecArrayList.indices){
+            val modeloImagenSel = imagenSelecArrayList[i]
+            val nombreImagen = modeloImagenSel.id
+            val rutaImagen = "Productos/$nombreImagen"
+
+            val storageRef = FirebaseStorage.getInstance().getReference(rutaImagen)
+            storageRef.putFile(modeloImagenSel.imageUri!!)
+
+                .addOnSuccessListener {taskSnapshot ->
+                    val uriTask = taskSnapshot.storage.downloadUrl
+                    while (!uriTask.isSuccessful);
+                    val uriImgCargada = uriTask.result
+
+                    if (uriTask.isSuccessful){
+                        val hashMap = HashMap<String, Any>()
+                        hashMap["id"] = "${modeloImagenSel.id}"
+                        hashMap["imagenUrl"] = "${uriImgCargada}"
+
+                        val ref = FirebaseDatabase.getInstance().getReference("Productos")
+                        ref.child(keyId).child("Imagenes")
+                            .child(nombreImagen)
+                            .updateChildren(hashMap)
+                        progressDialog.dismiss()
+                        Toast.makeText(this, "Se agrego el producto", Toast.LENGTH_SHORT).show()
+                        limpiarCampos()
+                    }
+
+
+                }
+
+                .addOnFailureListener {e->
+                    progressDialog.dismiss()
+                    Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+
+    }
+    private fun limpiarCampos() {
+        imagenSelecArrayList.clear()
+        adaptadorImagenSet.notifyDataSetChanged()
+        binding.etNombresP.setText("")
+        binding.etDescripcionP.setText("")
+        binding.etPrecioP.setText("")
+        binding.Categoria.setText("")
+        binding.descuentoSwitch.isChecked = false
+        binding.etPrecioConDescuentoP.setText("")
+        binding.etNotaDescuentoP.setText("")
+
+    }
+
+
 
     private fun cargarCategorias() {
 
@@ -85,7 +266,7 @@ class AgregarProductoActivity : AppCompatActivity() {
             .setItems(categoriasArray) { dialog, which ->
                 idCat = caegoriasArrayList[which].id
                 tituloCat = caegoriasArrayList[which].categoria
-                binding.Categria.text = tituloCat
+                binding.Categoria.text = tituloCat
             }
             .show()
     }
@@ -119,3 +300,8 @@ class AgregarProductoActivity : AppCompatActivity() {
             }
         }
 }
+
+
+
+
+
