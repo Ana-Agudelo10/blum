34e23c74ee49710d31ced3331da500b1d520732b
Esthefania <estefa.naravez.26@gmail.com>
2025-11-05 21:27:00 -0500

    Fase 2.Listar productos Blum

--- app/src/main/java/com/ecommerce/blum/vendedor/Bottom_nav_fragments_vendedor/FragmentMisProductosV.kt
+++ app/src/main/java/com/ecommerce/blum/vendedor/Bottom_nav_fragments_vendedor/FragmentMisProductosV.kt
@@ -1,20 +1,65 @@
 package com.ecommerce.blum.vendedor.Bottom_nav_fragments_vendedor
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ecommerce.blum.Adaptadores.AdaptadorProducto
+import com.ecommerce.blum.Modelos.ModeloProducto
 import com.ecommerce.blum.R
+import com.ecommerce.blum.databinding.FragmentMisProductosVBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class FragmentMisProductosV : Fragment() {
+    
+    private lateinit var binding : FragmentMisProductosVBinding
+    private lateinit var mContext: Context
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis_productos_v, container, false)
+    private lateinit var productoArrayList: ArrayList<ModeloProducto>
+    private lateinit var adaptadorProductos : AdaptadorProducto
+
+    override fun onAttach(context: Context) {
+
+        mContext = context
+        super.onAttach(context)
+    }
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+       binding = FragmentMisProductosVBinding.inflate(LayoutInflater.from(mContext), container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listarProductos()
+    }
+
+    private fun listarProductos() {
+        productoArrayList = ArrayList()
+
+        val ref = FirebaseDatabase.getInstance().getReference("Productos")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                productoArrayList.clear()
+                for(ds in snapshot.children){
+                    val modeloProducto = ds.getValue(ModeloProducto::class.java)
+                    productoArrayList.add(modeloProducto!!)
+
+                }
+                adaptadorProductos = AdaptadorProducto(mContext, productoArrayList)
+                binding.productosRv.adapter = adaptadorProductos
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
 }
